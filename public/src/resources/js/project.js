621cd6b549864e5120aa8460b229cb8953382926
namsam25 <namsam17@naver.com>
2017-06-19 13:14:48 +0900

    namsam

---
+++ public/src/resources/js/project.js
@@ -0,0 +1,21 @@
+
+$.fn.outerHTML = function(){
+	var th = $(this);
+	if( !th[0] ) return "";
+	if (th[0].outerHTML){
+		return th[0].outerHTML;
+	}else{
+		var content = th.wrap('<div>').parent().html();
+		th.unwrap();
+		return content;
+	}
+}
+
+namsam([
+	{
+		path : '',
+		controller : 'common'
+	}
+],{
+
+});
